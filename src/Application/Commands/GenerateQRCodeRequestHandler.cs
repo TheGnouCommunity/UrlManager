01480ebc9bc5a39faad3b143a11131f93506537d
Sébastien Boulay <boulc@boulc.com>
2024-04-08 18:24:08 -0400

    feat: add qr code generation

---
+++ src/Application/Commands/GenerateQRCodeRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Net.Codecrete.QrCodeGenerator;
+using System.Text;
+
+namespace TheGnouCommunity.UrlManager.Application.Commands;
+
+internal sealed class GenerateQRCodeRequestHandler : IRequestHandler<GenerateQRCodeRequest, Result<byte[]>>
+{
+    private readonly ILogger _logger;
+
+    public GenerateQRCodeRequestHandler(ILogger<GenerateQRCodeRequestHandler> logger)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+
+        _logger = logger;
+    }
+
+    public async Task<Result<byte[]>> Handle(GenerateQRCodeRequest request, CancellationToken cancellationToken)
+    {
+        var qrCode = QrCode.EncodeText(request.Url, QrCode.Ecc.Medium);
+        return Encoding.Default.GetBytes(qrCode.ToSvgString(4));
+    }
+}
